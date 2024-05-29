cd6c21e74b4cddecfc8531d5e12c7f2015975b48
nabilshukri <nabilshukri2001@gmail.com>
2024-05-29 11:59:49 +0800

    Initial commit

---
+++ src/main/java/com/prasarana/springbootprasarana/SpringbootprasaranaApplication.java
@@ -0,0 +1,13 @@
+package com.prasarana.springbootprasarana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprasaranaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprasaranaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prasarana/springbootprasarana/SpringbootprasaranaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasarana.springbootprasarana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprasaranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
