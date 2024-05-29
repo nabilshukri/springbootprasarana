c4e6ae64788f8bd88936cd195669678f53fb101b
nabilshukri <nabilshukri2001@gmail.com>
2024-05-29 18:08:35 +0800

    Hardcoded admin account in db using CommandLine

--- src/main/java/com/prasarana/springbootprasarana/SpringbootprasaranaApplication.java
+++ src/main/java/com/prasarana/springbootprasarana/SpringbootprasaranaApplication.java
@@ -1,13 +1,33 @@
 package com.prasarana.springbootprasarana;
 
+import com.prasarana.springbootprasarana.model.MyUser;
+import com.prasarana.springbootprasarana.model.MyUserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringbootprasaranaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootprasaranaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(MyUserRepository myUserRepository, PasswordEncoder passwordEncoder) {
+
+		return args ->{
+			MyUser user = new MyUser();
+			user.setUsername("admin");
+			user.setPassword(passwordEncoder.encode("admin"));
+			user.setRole("ADMIN");
+			myUserRepository.save(user);
+		};
 	}
 
 }
