97cc398cb6b9be5d69d2e7263e133f90256d939a
nabilshukri <nabilshukri2001@gmail.com>
2024-05-29 16:34:15 +0800

    Admin Access to both user and admin page

--- src/main/java/com/prasarana/springbootprasarana/SecurityConfiguration.java
+++ src/main/java/com/prasarana/springbootprasarana/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(registry -> {
                     registry.requestMatchers("/home", "/register", "/register/**").permitAll();
                     registry.requestMatchers("/admin/**").hasRole("ADMIN");
-                    registry.requestMatchers("/user/**").hasRole("USER");
+                    registry.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN");
                     registry.anyRequest().authenticated();
         })
                 .formLogin(httpSecurityFormLoginConfigurer -> {
